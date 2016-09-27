5a6daf8ddd6be00e5390eca5428148d36b846653
lishaowei <liwei980167048@qq.com>
2016-09-27 13:11:02 +0800

    ad

---
+++ controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sks
+ * Date: 2016/9/27
+ * Time: 13:09
+ */
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class IndexController extends Controller
+{
+    public function actionIndex()
+    {
+        echo 'hello world';
+    }
+}
